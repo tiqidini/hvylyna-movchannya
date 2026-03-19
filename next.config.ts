4002717b6ea6dd30ee53250c184ae0e3417bdb55
tiqidini <dubolazoff@gmail.com>
2026-03-19 21:42:41 +0200

    chore: setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/hvylyna-movchannya',
+  assetPrefix: '/hvylyna-movchannya',
+  images: {
+    unoptimized: true,
+  },
   reactCompiler: true,
 };
 
